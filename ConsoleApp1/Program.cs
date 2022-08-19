20de82c976ff5615658736bbc3b4c44a21b5bac8
Drew Nelson <andrewnelson@shoemakermfg.com>
2022-08-19 11:43:21 -0700

    check name and password completed

--- ConsoleApp1/Program.cs
+++ ConsoleApp1/Program.cs
@@ -4,7 +4,7 @@
     {
         // display title
         System.Console.WriteLine("*************** Nelson Bank ***************");
-        System.Console.WriteLine("::Login Page"::);
+        System.Console.WriteLine("::Login Page::");
 
         // declare variables to store username and password
         string userName = null, password = null;
@@ -13,10 +13,21 @@
         System.Console.Write("Username:");
         userName = System.Console.ReadLine(); // ReadLine will read the value from the keyboard and assign it to the userName variable
 
-        // read password from keyboard
-        System.Console.Write("Password:");
-        password = System.Console.ReadLine();
+        // read password from keyboard only if username is entered
+        if (userName != "")
+        {
+            System.Console.Write("Password:");
+            password = System.Console.ReadLine();
+        }
 
+        // check username and passwork
+        if (userName == "system" && password == "manager")
+        {
+            System.Console.WriteLine("TO DO: Main menu here");
+        }
 
+        //about to exit
+        System.Console.Write("Thank you! Please visit us again.");
+        System.Console.ReadKey();
     }
 }
